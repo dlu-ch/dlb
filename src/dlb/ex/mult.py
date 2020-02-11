3fb51b5cfff765b65aa832bfa588e1011d4a1d3d
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-02-11 14:09:37 +0100

    Add dlb.ex.mult.{MultiplicityRange, MultiplicityHolder}

---
+++ src/dlb/ex/mult.py
@@ -0,0 +1,125 @@
+import typing
+
+
+class MultiplicityRange:
+    # Describes a set of multiplicity by an integer or a slice of integers.
+    #
+    # If MultiplicityRange(m) is valid:
+    # An integer ``n`` is considered a multiplicity in the range iff ``n in [i for i in range(n + 1)[m]``.
+
+    def __init__(self, multiplicity=typing.Union[int, slice]):
+        if isinstance(multiplicity, int):
+            multiplicity = slice(multiplicity, multiplicity + 1)
+
+        try:
+            if not isinstance(multiplicity, slice):
+                raise TypeError
+            start = multiplicity.start
+            stop = multiplicity.stop
+            step = multiplicity.step
+            if not all(e is None or isinstance(e, int) for e in (start, stop, step)):
+                raise TypeError
+        except TypeError:
+            raise TypeError(f"'multiplicity' must be int or slice of int, not {multiplicity!r}") from None
+
+        step = 1 if step is None else step
+        if step <= 0:
+            raise ValueError(f'slice step must be positive, not {step}')
+
+        start = 0 if start is None else start
+        if start < 0:
+            raise ValueError(f'minimum multiplicity (start of slice) must be non-negative, not {start}')
+
+        if stop is not None and stop < 0:
+            raise ValueError(f'upper multiplicity bound (stop of slice) must be non-negative, not {stop}')
+
+        if stop is not None:
+            if stop > start:
+                c = (stop - start - 1) // step  # number of repetitions (except start)
+                stop = c * step + start + 1  # make stop the maximum + 1
+                if c == 0:
+                    stop = start + 1
+                    step = 1
+            else:  # empty
+                start = 0
+                stop = 0
+                step = 1
+
+        self._slice = slice(start, stop, step)
+
+    def __contains__(self, count):
+        if not isinstance(count, int):
+            raise TypeError("'count' must be integer")
+
+        s = self._slice
+        if count < s.start:
+            return False
+        if s.stop is not None and count >= s.stop:
+            return False
+        if (count - s.start) % s.step != 0:
+            return False
+
+        return True
+
+    @property
+    def as_slice(self):
+        return self._slice
+
+    def __eq__(self, other):
+        return self.as_slice == other.as_slice
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        s = self._slice
+
+        if s.stop is not None and s.stop == s.start + 1:
+            suffix = str(s.start)  # exactly one member
+        else:
+            suffix = (str(s.start) if s.start else '') + ':' + (str(s.stop) if s.stop is not None else '')
+            if s.step > 1:
+                suffix += ':' + str(s.step)
+
+        return f'[{suffix}]'
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._slice})"
+
+
+class _MultiplicityHolderProxy:
+    def __init__(self, element_class: type, multiplicity: typing.Optional[MultiplicityRange]):
+        self._element_class = element_class
+        self._multiplicity = multiplicity
+
+    @property
+    def element_class(self):
+        return self._element_class
+
+    def __call__(self, *args, **kwargs):  # simulate a constructor call
+        element = self._element_class(*args, **kwargs)
+        element._multiplicity = self._multiplicity
+        return element
+
+    def __getitem__(self, multiplicity):
+        raise TypeError(f'{self._element_class.__name__!r} with multiplicity is not subscriptable')
+
+
+class _MultiplicitiyHolderMeta(type):
+    def __getitem__(cls, multiplicity: typing.Union[int, slice]) -> _MultiplicityHolderProxy:
+        return _MultiplicityHolderProxy(cls, MultiplicityRange(multiplicity))
+
+
+class MultiplicityHolder(metaclass=_MultiplicitiyHolderMeta):
+    # A subclass T of _Multiplicity can be instantiated like this:
+    #
+    #   T(a=1)     ->   instance t of T with t.multiplicity = None
+    #   T[m](a=1)  ->   instance t of T with t.multiplicity = Multiplicity(m)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._multiplicity = None
+
+    @property
+    def multiplicity(self):
+        return self._multiplicity
---
+++ test/test_mult.py
@@ -0,0 +1,161 @@
+import sys
+import os.path
+here = os.path.dirname(__file__) or os.curdir
+sys.path.insert(0, os.path.abspath(os.path.join(here, '../src')))
+
+import dlb.ex.mult
+import unittest
+import random
+
+
+class TestConstruction(unittest.TestCase):
+
+    def test_succeeds_from_nonnegative_int(self):
+        m = dlb.ex.mult.MultiplicityRange(3)
+        self.assertEqual(slice(3, 3 + 1, 1), m.as_slice)
+
+        m = dlb.ex.mult.MultiplicityRange(0)
+        self.assertEqual(slice(0, 1, 1), m.as_slice)
+
+    def test_succeeds_from_slice(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(1, 20, 3))
+        self.assertEqual(slice(1, 20, 3), m.as_slice)
+
+    def test_fails_from_none(self):
+        with self.assertRaises(TypeError) as cm:
+            dlb.ex.mult.MultiplicityRange(None)
+        self.assertEqual("'multiplicity' must be int or slice of int, not None", str(cm.exception))
+
+    def test_fails_from_negative_int(self):
+        with self.assertRaises(ValueError) as cm:
+            dlb.ex.mult.MultiplicityRange(-1)
+        self.assertEqual("minimum multiplicity (start of slice) must be non-negative, not -1", str(cm.exception))
+
+    def test_fails_from_slice_with_negative_elementstart_stop_or_step(self):
+        with self.assertRaises(ValueError) as cm:
+            dlb.ex.mult.MultiplicityRange(slice(-1, 20, 3))
+        self.assertEqual("minimum multiplicity (start of slice) must be non-negative, not -1", str(cm.exception))
+
+        with self.assertRaises(ValueError) as cm:
+            dlb.ex.mult.MultiplicityRange(slice(1, -20, 3))
+        self.assertEqual("upper multiplicity bound (stop of slice) must be non-negative, not -20", str(cm.exception))
+
+        with self.assertRaises(ValueError) as cm:
+            dlb.ex.mult.MultiplicityRange(slice(1, 20, -3))
+        self.assertEqual("slice step must be positive, not -3", str(cm.exception))
+
+    def test_fails_from_noninteger_slice(self):
+        with self.assertRaises(ValueError) as cm:
+            dlb.ex.mult.MultiplicityRange(slice('1', 20, 3))
+        self.assertEqual("slice step must be positive, not -3", str(cm.exception))
+
+    def test_fails_from_noninteger_slice(self):
+        with self.assertRaises(TypeError) as cm:
+            dlb.ex.mult.MultiplicityRange(slice('1', 20, 3))
+        self.assertEqual("'multiplicity' must be int or slice of int, not slice('1', 20, 3)", str(cm.exception))
+
+    def test_normalizes_empty_slice(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(3, 3, 20))
+        self.assertEqual(m.as_slice, slice(0, 0, 1))
+
+    def test_normalizes_slice_with_only_one_member(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(3, 23, 20))
+        self.assertEqual(m.as_slice, slice(3, 4, 1))
+
+
+class TestCompare(unittest.TestCase):
+
+    def test_normalized_equal_are_equal(self):
+        m1 = dlb.ex.mult.MultiplicityRange(slice(2, 2))
+        m2 = dlb.ex.mult.MultiplicityRange(slice(5, 2, 7))
+        self.assertTrue(m1 == m2)
+        self.assertFalse(m1 != m2)
+
+        m1 = dlb.ex.mult.MultiplicityRange(5)
+        m2 = dlb.ex.mult.MultiplicityRange(slice(5, 6, 7))
+        self.assertTrue(m1 == m2)
+        self.assertFalse(m1 != m2)
+
+    def test_different_are_not_equal(self):
+        m1 = dlb.ex.mult.MultiplicityRange(3)
+        m2 = dlb.ex.mult.MultiplicityRange(4)
+        self.assertTrue(m1 != m2)
+        self.assertFalse(m1 == m2)
+
+
+class TestStr(unittest.TestCase):
+
+    def test_empty_is_correct(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(2, 2))
+        self.assertEqual('[:0]', str(m))
+
+    def test_minimum_is_correct(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(3, None))
+        self.assertEqual('[3:]', str(m))
+
+    def test_upper_bound_is_correct(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(None, 4))
+        self.assertEqual('[:4]', str(m))
+
+    def test_unrestricted_is_correct(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(None))
+        self.assertEqual('[:]', str(m))
+
+
+class TestRepr(unittest.TestCase):
+
+    def test_upper_bound_is_correct(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(None, 4))
+        self.assertEqual('MultiplicityRange(slice(0, 4, 1))', repr(m))
+
+
+class TestMatchesCount(unittest.TestCase):
+
+    def test_integer_matches_exact_count(self):
+        m = dlb.ex.mult.MultiplicityRange(2)
+        self.assertTrue(2 in m)
+        self.assertFalse(1 in m)
+        self.assertFalse(3 in m)
+
+    def test_empty_slice_matches_nothing(self):
+        m = dlb.ex.mult.MultiplicityRange(slice(0, 0, 4))
+        self.assertFalse(0 in m)
+        self.assertFalse(2 in m)
+
+    def test_slice_matches_like_builtin(self):
+        top = 50
+
+        for i in range(1000):
+            start = random.randrange(-1, top)
+            stop = random.randrange(-1, top)
+            step = random.randrange(0, top // 10)
+            i = tuple(i for i in range(top + 1))
+            try:
+                s = slice(start, stop, step)
+                m = dlb.ex.mult.MultiplicityRange(s)
+                for j in range(100):
+                    n = random.randrange(start, stop + 1)
+                    self.assertEqual(n in i[s], n in m, f'{n} in {s}?')
+            except ValueError:
+                pass
+
+
+class TestMultiplicityHolder(unittest.TestCase):
+
+    class M(dlb.ex.mult.MultiplicityHolder):
+        def __init__(self, a, b=1):
+            super().__init__()
+
+    def test_has_no_multiplicity_when_constructed_directly(self):
+        m = TestMultiplicityHolder.M(1, b=2)
+        self.assertIsInstance(m, TestMultiplicityHolder.M)
+        self.assertIsNone(m.multiplicity)
+
+    def test_has_multiplicity_when_constructed_by_slice(self):
+        m = TestMultiplicityHolder.M[:3](1, b=2)
+        self.assertIsInstance(m, TestMultiplicityHolder.M)
+        self.assertEqual(dlb.ex.mult.MultiplicityRange(slice(0, 3)), m.multiplicity)
+
+    def test_multiplicity_cannot_be_assigned(self):
+        with self.assertRaises(AttributeError):
+            TestMultiplicityHolder.M(1, b=2).multiplicity = None
