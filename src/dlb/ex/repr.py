32097a4f363ec8b5c63b772c55f7f4f1433e7506
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-02-11 14:31:18 +0100

    fixup! Add dlb.ex.repr.make_fundamental()

--- src/dlb/ex/repr.py
+++ src/dlb/ex/repr.py
@@ -13,42 +13,38 @@ def is_immutable_fundamental(obj):
     return obj is None or isinstance(obj, _non_container_fundamental_types)
 
 
-def _make_fundamental(obj, remaining_depth, force_ordered):
-    if remaining_depth <= 0:
-        raise ValueError
-
+def _make_fundamental(obj, force_ordered):
     if is_immutable_fundamental(obj):
         return obj
 
-    n = remaining_depth - 1
     o = force_ordered
 
     if isinstance(obj, collections.abc.Mapping):  # note: loses order of collections.OrderedDict
         if o:
-            return tuple(sorted((_make_fundamental(k, n, o), _make_fundamental(v, n, o)) for k, v in obj.items()))
-        return {_make_fundamental(k, n, o): _make_fundamental(v, n, o) for k, v in obj.items()}
+            return tuple(sorted((_make_fundamental(k, o), _make_fundamental(v, o)) for k, v in obj.items()))
+        return {_make_fundamental(k, o): _make_fundamental(v, o) for k, v in obj.items()}
 
     if isinstance(obj, (set, frozenset)):
-        obj = frozenset(_make_fundamental(k, n, o) for k in obj)
+        obj = frozenset(_make_fundamental(k, o) for k in obj)
         if not o:
             return obj
         return tuple(sorted(obj))
 
     if isinstance(obj, collections.abc.Iterable):
-        return tuple(_make_fundamental(k, n, o) for k in obj)
+        return tuple(_make_fundamental(k, o) for k in obj)
 
     raise TypeError
 
 
 def make_fundamental(obj, replace_unordered_by_tuple=False):
-    max_nesting_depth = 1001
     try:
-        return _make_fundamental(obj, max_nesting_depth, replace_unordered_by_tuple)
-    except:
+        # note: isinstance() can lead to infinite recursion, aborted by RecursionError
+        return _make_fundamental(obj, replace_unordered_by_tuple)
+    except (TypeError, ValueError, RecursionError):
         f = ', '.join(repr(c.__name__) for c in _non_container_fundamental_types)
         msg = (
             f"cannot be made fundamental: {obj!r}\n"
             f"  | an object is fundamental if it is 'None', or of type {f}, "
-            f"or an iterable of only such objects (nested to at most {max_nesting_depth} levels)"
+            f"or an iterable of only such objects"
         )
         raise TypeError(msg) from None
