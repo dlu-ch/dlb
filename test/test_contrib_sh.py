ffe3231ce8bf339d0b37cfe38a86aaf00648b3e4
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-03-11 17:57:56 +0100

    Add dlb_contrib_sh.ShScriptlet

---
+++ test/test_contrib_sh.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# dlb - a Pythonic build tool
+# Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+import sys
+import os.path
+here = os.path.dirname(__file__) or os.curdir
+sys.path.insert(0, os.path.abspath(os.path.join(here)))
+sys.path.insert(0, os.path.abspath(os.path.join(here, '../src')))
+
+import dlb.fs
+import dlb.di
+import dlb.ex
+import dlb_contrib_sh
+import unittest
+from typing import Iterable, Union
+import tools_for_test
+
+
+class OutputTwoLines(dlb_contrib_sh.ShScriptlet):
+    SCRIPTLET = """
+        echo first
+        echo second
+        """
+
+
+class ReadFile(dlb_contrib_sh.ShScriptlet):
+    SCRIPTLET = """
+        echo $0
+        echo "$@"
+        """
+
+    source_files = dlb.ex.Tool.Input.RegularFile[:]()
+
+    def get_compile_arguments(self) -> Iterable[Union[str, dlb.fs.Path, dlb.fs.Path.Native]]:
+        return [s for s in self.source_files]
+
+
+@unittest.skipIf(not os.path.isfile('/bin/sh'), 'requires SH')
+class ShTest(tools_for_test.TemporaryWorkingDirectoryTestCase):
+
+    def test_line_output(self):
+        with dlb.ex.Context(find_helpers=True):
+            output = OutputTwoLines().run().output
+        self.assertEqual('first\nsecond\n', output)
+
+    def test_read_files(self):
+        open('a', 'xb').close()
+        open('b', 'xb').close()
+
+        with dlb.di.Cluster('let sh output all parameters'), dlb.ex.Context(find_helpers=True):
+            output = ReadFile(source_files=['a', 'b']).run().output
+            dlb.di.inform(f"scriptlet returned {output!r}")
