4c0deec5fbd42287a335a5e43aed75e163d29fca
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-04-07 10:26:43 +0200

    Add dlb_contrib.partition

---
+++ src/dlb_contrib/partition.py
@@ -0,0 +1,87 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# dlb - a Pythonic build tool
+# Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+"""Partition a set of files in the managed tree."""
+
+# Usage example:
+#
+#   import dlb.fs
+#   import dlb_contrib.partition
+#
+#   paths = dlb.fs.Path(...).list(...)
+#   path_groups = dlb_contrib.partition.by_working_tree_path(paths, number_of_groups=1000)
+#   # path_groups now contains (at most 1000) non-empty lists of elements of *paths*
+#
+#   path_groups = dlb_contrib.partition.split_longer(path_groups, max_length=5)
+#   # path_groups now contains (non-empty) lists with at most 5 members
+
+__all__ = ['by_working_tree_path', 'split_longer']
+
+import sys
+import zlib
+import dlb.fs
+import dlb.ex
+from typing import Any, Iterable, List, Sequence
+assert sys.version_info >= (3, 7)
+
+
+def by_working_tree_path(paths: Sequence[dlb.fs.PathLike], *, number_of_groups: int, collapsable=False) \
+        -> List[List[dlb.fs.Path]]:
+
+    # Partition *paths* into *number_of_groups* groups based on their working tree path in a stable and portable way
+    # (the result is the same for the same paths on all supported platforms, Python versions and dlb runs).
+    #
+    # If *paths* contains multiple paths with the same working tree path, all of them are placed in the same group.
+    # Otherwise, the length of the groups is "similar*.
+    # More precisely:
+    # For *m* > 0 elements in *paths* and *number_of_groups* >= 1, all with different and uniformely distributed hashes
+    # of their working tree paths, the expected value of the length of each group is *m* / *number_of_groups*.
+    #
+    # If *collapsable* is True, all paths in *paths* are assumed to be collapsable (only relevant if at least one of
+    # them contains a '..' component).
+    #
+    # Returns a list of at most *number_of_groups* non-empty lists of elements of *paths* that together contain all
+    # elements of *path* if *number_of_groups* > 0 and [] otherwise.
+
+    number_of_groups = int(number_of_groups)
+    collapsable = bool(collapsable)
+    context = dlb.ex.Context.active
+
+    working_tree_paths = [
+        context.working_tree_path_of(p, allow_temporary=True, existing=True, collapsable=collapsable)
+        for p in paths
+    ]
+
+    if not paths:
+        return []
+
+    if number_of_groups <= 1:
+        return [paths]
+
+    groups = [[] for _ in range(number_of_groups)]
+    for i in range(len(paths)):
+        b = working_tree_paths[i].as_string().encode()
+        h = zlib.crc32(b, zlib.crc32(b))  # concatenation makes reduces runs for short *b* in small *number_of_groups*
+        groups[h % number_of_groups].append(paths[i])
+
+    return [g for g in groups if g]
+
+
+def split_longer(sequence: Iterable[Sequence[Any]], *, max_length: int) -> List[List[Any]]:
+    # Return a list of all members of *sequence* as lists, split into list of at most *max_length* members if longer.
+    # Order is preserved.
+    #
+    # Examples:
+    #
+    #   split_longer([], max_length=3)                              # []
+    #   split_longer([[1, 2], [3, 4, 5, 6, 7], [8]], max_length=3)  # [[1, 2], [3, 4, 5], [6, 7], [8]]
+
+    max_length = max(1, int(max_length))
+    list_of_smaller = []
+    for seq in sequence:
+        seq = list(seq)
+        while seq:
+            list_of_smaller.append(seq[:max_length])
+            seq = seq[max_length:]
+    return list_of_smaller
---
+++ test/test_contrib_partition.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# dlb - a Pythonic build tool
+# Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+import sys
+import os.path
+here = os.path.dirname(__file__) or os.curdir
+sys.path.insert(0, os.path.abspath(os.path.join(here)))
+sys.path.insert(0, os.path.abspath(os.path.join(here, '../src')))
+
+import dlb.ex
+import dlb_contrib.partition
+import os
+import unittest
+import tools_for_test
+
+
+class ByWorkingTreePathTest(tools_for_test.TemporaryWorkingDirectoryTestCase):
+
+    def test_empty_for_empty(self):
+        with dlb.ex.Context():
+            self.assertEqual([], dlb_contrib.partition.by_working_tree_path([], number_of_groups=3))
+
+    def test_all_in_same_group_for_nonpositive(self):
+        paths = ['a', dlb.fs.Path('b')]
+        with dlb.ex.Context():
+            self.assertEqual([paths], dlb_contrib.partition.by_working_tree_path(paths, number_of_groups=0))
+
+    def test_similar_in_same_group(self):
+        paths = ['a', dlb.fs.Path('a/'), 'a', './a', 'a////', ('', 'a')]
+        groups = [
+            [dlb.fs.Path('a/'), 'a////'],
+            ['a', 'a', './a', ('', 'a')]
+        ]
+        with dlb.ex.Context():
+            self.assertEqual(groups, dlb_contrib.partition.by_working_tree_path(paths, number_of_groups=2))
+
+    def test_distributes_well_for_small_number(self):
+        paths = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+        with dlb.ex.Context():
+            groups = dlb_contrib.partition.by_working_tree_path(paths, number_of_groups=2)
+        self.assertEqual(len(paths), sum(len(g) for g in groups))
+
+        group_indices = []
+        for p in paths:
+            for i, g in enumerate(groups):
+                if p in g:
+                    group_indices.append(i)
+                    break
+        self.assertEqual(len(paths), len(group_indices))
+        starts_of_run = [j for j, i in enumerate(group_indices[:-1]) if i == group_indices[j + 1]]
+
+        self.assertLessEqual(len(starts_of_run), 1, starts_of_run)
+
+
+class SplitLongerTest(unittest.TestCase):
+
+    def test_empty_for_empty(self):
+        self.assertEqual([], dlb_contrib.partition.split_longer([], max_length=3))
+
+    def test_splits_longer(self):
+        self.assertEqual([[1, 2], [3, 4, 5], [6, 7], [8]],
+                         dlb_contrib.partition.split_longer([[1, 2], [3, 4, 5, 6, 7], [8]], max_length=3))
