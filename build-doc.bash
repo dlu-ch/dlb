1f0cb3e829cb24bf03357e55500be5cabdb2c268
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-04-15 21:46:48 +0200

    Improve Bash scripts
     - choose Sphinx builders by command-line arguments
     - clean directory before setup.py

--- build-doc.bash
+++ build-doc.bash
@@ -9,10 +9,19 @@ doc_to_root=..
 out_dir=build/out
 sphinx_out_dir="${out_dir:?}/sphinx"
 
+# list of builders: https://www.sphinx-doc.org/en/master/usage/builders/index.html
+# most important: html, linkcheck
+builders=("$@")
+if [ "${#builders[@]}" -eq 0 ]; then
+    builders=(html)
+fi
+
 (
     cd "./${doc_dir:?}"  # as readthedocs does it
     rm -rf -- "./${doc_to_root:?}/${sphinx_out_dir:?}"
     sphinx_args=("-d" "./${doc_to_root:?}/${sphinx_out_dir:?}/doctrees")
-    "${SPHINXBUILD:?}" "${sphinx_args[@]}" -b html . "./${doc_to_root:?}/${sphinx_out_dir:?}/html"
-    "${SPHINXBUILD:?}" "${sphinx_args[@]}" -b linkcheck  . "./${doc_to_root:?}/${sphinx_out_dir:?}/linkcheck"
+    for builder in "${builders[@]}"; do
+        "${SPHINXBUILD:?}" "${sphinx_args[@]}" -b "${builder:?}" \
+            . "./${doc_to_root:?}/${sphinx_out_dir:?}/${builder:?}"
+    done
 )
--- build-package.bash
+++ build-package.bash
@@ -2,9 +2,12 @@
 
 set -e
 
+RM=rm
 PYTHON=python3
 out_dir=build/out
+setup_build_dir="${out_dir}/setupbuild"
 
 # may require this before: pip3 install wheel
 # upload to PyPI with: twine upload dist/dlb-*.whl
-"${PYTHON:?}" setup.py build --build-base="${out_dir}/setupbuild" bdist_wheel --dist-dir=dist
+"${RM:?}" -rf -- "${setup_build_dir:?}"
+"${PYTHON:?}" setup.py build --build-base="${setup_build_dir:?}" bdist_wheel --dist-dir=dist
