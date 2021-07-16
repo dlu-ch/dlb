92b5fd7dc3ad7c16a710404c028f5eaaa411fd10
Daniel Lutz <dlu-ch@users.noreply.github.com>
2021-07-16 13:26:24 +0200

    Add test for dlb_launcher

---
+++ test/dlb/test_launcher_with_different_dlb.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+PYTHON3=python3
+READLINK=readlink
+
+script_dir="$("${READLINK:?}" -e -- "$0")"
+script_dir="${script_dir%/*}"
+path_to_root=../..
+cd -- "${script_dir:?}"
+
+(
+    trap 'echo error: ${0##*/} aborted' EXIT
+    cd data/fakedlb
+    path_to_root="../../${path_to_root:?}"
+    script_output="$(PYTHONPATH="${path_to_root:?}/src" "${PYTHON3:?}" -m dlb_launcher script 2>/dev/null)"
+    if [ "${script_output}" != "successfully completed." ]; then
+        printf "unexpected script output: %q\n" "${script_output}"
+        exit 1
+    fi
+    trap - EXIT
+)
+
+echo "${0##*/}: completed successfully"
