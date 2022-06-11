df284ec2a028d5b1e5b0a4fb6c8ab32bb17b2979
Daniel Lutz <dlu-ch@users.noreply.github.com>
2022-06-11 18:34:17 +0200

    Install and run Sphinx in virtual environment

--- build-doc.bash
+++ build-doc.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Build documentation with Sphinx <https://www.sphinx-doc.org> in a virtual Python environment.
+# Installs required packages from PyPI according to 'doc/requirements.txt'.
+# Required Python version: see 'doc/requirements.txt'.
+
 set -e
 
-SPHINXBUILD=sphinx-build
+PYTHON3=python3
+DOT=dot  # required by sphinx.ext.graphviz, sphinx.ext.inheritance_diagram
+RM=rm
+CP=cp
+MV=mv
+CMP=cmp
 
 script_dir="/${0:?}"
 script_dir="${script_dir%/*}"
@@ -14,6 +23,37 @@ doc_dir=doc
 doc_to_root=..
 out_dir=build/out
 sphinx_out_dir="${out_dir:?}/sphinx"
+sphinxvenv_dir="${out_dir:?}/sphinxvenv"
+
+if [ -f "${sphinxvenv_dir:?}/requirements.txt" ] \
+    && "${CMP:?}" -- "${doc_dir:?}/requirements.txt" "${sphinxvenv_dir:?}/requirements.txt";
+then
+    printf '(re)use virtual environment: %q\n' "${sphinxvenv_dir:?}/bin/activate"
+    source -- "${sphinxvenv_dir:?}/bin/activate"
+else
+    printf '(re)create virtual environment: %q\n' "${sphinxvenv_dir:?}/bin/activate"
+    "${RM:?}" -rf -- "./${sphinxvenv_dir:?}"
+    "${PYTHON3:?}" -m venv "./${sphinxvenv_dir:?}"
+    source -- "${sphinxvenv_dir:?}/bin/activate"
+    pip install --upgrade -r "./${doc_dir:?}/requirements.txt"
+    "${CP:?}" -- "${doc_dir:?}/requirements.txt" "${sphinxvenv_dir:?}/requirements.txt.t"
+    "${MV:?}" -- "${sphinxvenv_dir:?}/requirements.txt.t" "${sphinxvenv_dir:?}/requirements.txt"
+fi
+
+sphinxbuild_file="$(command -v sphinx-build || echo)"
+sphinxvenv_abs_dir="${PWD:?}/${sphinxvenv_dir:?}"
+if ! [ -n "${sphinxbuild_file}" -a -f "${sphinxbuild_file}" -a \
+    "${sphinxbuild_file:0:${#sphinxvenv_abs_dir}+1}" == "${sphinxvenv_abs_dir:?}/" ];
+then
+    printf "error: 'sphinx-build' not found in virtual environment: %q\n" "${sphinxvenv_dir:?}" >&2
+    exit 1
+fi
+
+dot_file="$(command -v dot || echo)"
+if ! [ -n "${dot_file}" -a -f "${dot_file}" ]; then
+    printf "error: 'dot' not found\n" >&2
+    exit 1
+fi
 
 # list of builders: https://www.sphinx-doc.org/en/master/usage/builders/index.html
 # most important: html, linkcheck, man
@@ -24,10 +64,11 @@ fi
 
 (
     cd "./${doc_dir:?}"  # as readthedocs does it
-    rm -rf -- "./${doc_to_root:?}/${sphinx_out_dir:?}"
+    "${RM:?}" -rf -- "./${doc_to_root:?}/${sphinx_out_dir:?}"
     sphinx_args=("-d" "./${doc_to_root:?}/${sphinx_out_dir:?}/doctrees")
     for builder in "${builders[@]}"; do
-        "${SPHINXBUILD:?}" "${sphinx_args[@]}" -b "${builder:?}" \
+        "${sphinxbuild_file:?}" "${sphinx_args[@]}" -b "${builder:?}" \
+            -D graphviz_dot="${dot_file:?}" \
             . "./${doc_to_root:?}/${sphinx_out_dir:?}/${builder:?}"
     done
 )
