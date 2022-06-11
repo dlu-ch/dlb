c0b35af489536699d505e5e8878bccc38494bf90
Daniel Lutz <dlu-ch@users.noreply.github.com>
2022-06-11 23:22:09 +0200

    Update to Sphinx 5.0.0

--- doc/conf.py
+++ doc/conf.py
@@ -76,7 +76,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -228,7 +228,10 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3.7', None)}
 # -- Options for sphinx.ext.extlinks --------------------------------------
 
 extlinks = {
-    'dlbrepo': (github_tree_url.replace('%', '%%') + '%s', '')  # usage: :dlbrepo:`example/`
+    'dlbrepo': (
+        github_tree_url.replace('%', '%%') + '%s',
+        None  # caption of link
+    )  # usage: :dlbrepo:`example/`
 }
 
 # -- Options for sphinx.ext.graphviz --------------------------------------
