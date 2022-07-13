ad0aac6d0ec5a99c819ccd475dbc5dfaccd2f362
Daniel Lutz <dlu-ch@users.noreply.github.com>
2022-07-13 09:30:45 +0200

    fixup! Fix typos in comments and documentation

--- test/dlb/testenv.py
+++ test/dlb/testenv.py
@@ -4,7 +4,6 @@ import os.path
 here = os.path.dirname(__file__) or os.curdir
 sys.path.insert(0, os.path.join(here, '..', '..', 'src'))
 sys.path.insert(0, os.path.join(here, '..'))
-# make sure sys.path does not a relative path before you import a module inside
 sys.path = [os.path.abspath(p) for p in sys.path]
 
 from testtool import *
